14bb6b18e4de1d35816ca3bc6c86955ec475004d
Samuelsonvs <m.sametatali@gmail.com>
2021-06-06 22:59:45 +0300

    review

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
 import Ads from "../components/Ads";
 import About from "../components/About";
 import Navbar from "../components/Navbar";
